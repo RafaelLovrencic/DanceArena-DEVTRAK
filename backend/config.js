cc35d5a35832440bdcbdd3c17e46a6150b6a39e6
alenjurina <alen.jurina@fer.hr>
2025-12-16 21:19:01 +0100

    Popravljen krivi URL

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 const FRONTEND_URL = process.env.NODE_ENV === "production"
     ? "https://dance-arena-devtrak.vercel.app"
-    : "https://localhost:5173";
+    : "http://localhost:5173";
 
 module.exports = { FRONTEND_URL };
