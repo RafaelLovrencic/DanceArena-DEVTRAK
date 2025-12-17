9ff76b8c00d62105e565eaea8d502ea6dd9f2917
alenjurina <alen.jurina@fer.hr>
2025-12-17 14:00:37 +0100

    Refs #19: Dodano sucelje za profil

--- frontend/src/stranice/profil.jsx
+++ frontend/src/stranice/profil.jsx
@@ -1,19 +1,19 @@
-import NavigacijskaTraka from "./navigacijskatraka.jsx";
 import { useAuth } from "../kontekst/AuthContext";
+import "../izgled/profil.css";
 
-export default function Profil(){
+export default function Profil({ onClose }){
     const { korisnik, odjava, loading } = useAuth();
     if (loading) return <p>Učitavanje...</p>;
     return (
         <>
-            <nav>
-                <NavigacijskaTraka />
-            </nav>
-            <div className="korisnikPodaci">
-            <p>Ime i prezime: {korisnik.ime}</p>
-            <p>Email: {korisnik.email}</p>
-            <p>Uloga: {korisnik.role || "Nije odabrana"}</p>
-            <button onClick={odjava}>Odjavi se</button>
+            <div className="profilSucelje">
+                <p>Ime i prezime: {korisnik.ime}</p>
+                <p>Email: {korisnik.email}</p>
+                <p>Uloga: {korisnik.role || "Nije odabrana"}</p>
+                <div className="profilTipke">
+                    <button onClick={odjava}>Odjavi se</button>
+                    <button type='button' onClick={onClose}>Odustani</button>
+                </div>
             </div>
         </>
     )
