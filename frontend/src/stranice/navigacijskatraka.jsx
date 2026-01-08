58f8dc8477cca78efdb5266157fb40cc5197034c
alenjurina <alen.jurina@fer.hr>
2026-01-08 21:04:08 +0100

    Refs #24: Popravak prikaza sucelja

--- frontend/src/stranice/navigacijskatraka.jsx
+++ frontend/src/stranice/navigacijskatraka.jsx
@@ -1,6 +1,7 @@
 import '../izgled/navigacijskatraka.css'
 import logo from '../izgled/pozadine/logo.png';
 import profil from '../izgled/pozadine/profilIkona.png';
+import Profil from './profil.jsx'
 import { Link, useLocation } from 'react-router-dom';
 import { useState } from 'react';
 import { BACKEND_IP } from '../config.js';
@@ -10,6 +11,7 @@ export default function NavigacijskaTraka() {
   const [burgerKlasa, setBurgerKlasa] = useState("burgerBar nekliknut");
   const [meniKlasa, setMeniKlasa] = useState("meni skriven");
   const [meniAktivan, setMeniAktivan] = useState(false);
+  const [pokaziProfil, setPokaziProfil] = useState(false);
   const { korisnik } = useAuth();
 
   const azurirajMeni = () => {
@@ -32,11 +34,11 @@ export default function NavigacijskaTraka() {
       <p className='DanceArena'>DanceArena</p>
     </div>
     <div className='tipke'>
-      {!korisnik && lokacija.pathname !== '/unospodataka' && lokacija.pathname !== 'profil' && (
+      {!korisnik && lokacija.pathname !== '/unospodataka' && (
         <button className="prijava" onClick={() => window.location.href=`${BACKEND_IP}/auth/google`}>Prijava</button>
       )}
-      {korisnik && lokacija.pathname !== '/unospodataka' && lokacija.pathname !== '/profil' && (
-        <Link to="/profil" className='profil'><img src={profil} alt="Profil" className='profilImg'/></Link>
+      {korisnik && lokacija.pathname !== '/unospodataka' && (
+        <button onClick={() => setPokaziProfil(true)} className='profil'><img src={profil} alt="Profil" className='profilImg'/></button>
       )}
       {/* {lokacija.pathname !== '/unospodataka' && (
         <div className='burgerMeni' onClick={azurirajMeni}>
@@ -47,6 +49,12 @@ export default function NavigacijskaTraka() {
       )} */}
       </div>
     <div className={meniKlasa}></div>
+    {pokaziProfil && (
+          <Profil onClose={() => {
+                    setPokaziProfil(false);
+            }}
+          />
+    )}
   </>
   )
 }
