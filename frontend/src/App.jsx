bc99de1ca17da32074f41da78c12d61f98e07bb5
RafaelLovrencic <rafael.lovrencic@fer.unizg.hr>
2026-01-10 12:00:39 +0100

    Ažuriranje App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Naslovnica from './stranice/naslovnica.jsx'
 import UnosPodataka from './stranice/unospodataka.jsx';
-<<<<<<< HEAD
-=======
 import Profil from './stranice/profil.jsx';
 import ObavijestSucu from './stranice/obavijestSucu.jsx';
->>>>>>> feature/22-mail
 import { AuthProvider } from "./kontekst/AuthContext";
 import Natjecanja from './stranice/natjecanja.jsx'
 import UnosPodatakaSudac from './stranice/unospodatakaSudac.jsx'
@@ -17,11 +14,8 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Naslovnica />} />
           <Route path="/unospodataka" element={<UnosPodataka />} />
-<<<<<<< HEAD
-=======
           <Route path="/unospodatakasuci" element={<UnosPodatakaSudac />} />
           <Route path="/profil" element={<Profil />} />
->>>>>>> feature/22-mail
           <Route path="/natjecanja" element={<Natjecanja />} />
           <Route path="/obavijestsucu" element={<ObavijestSucu />} />
       </Routes>
