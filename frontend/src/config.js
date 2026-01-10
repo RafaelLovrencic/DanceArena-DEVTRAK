416d202c8ec1401bce72e0e4d98aae109321d30f
RafaelLovrencic <rafael.lovrencic@fer.unizg.hr>
2026-01-10 11:13:40 +0100

    Rollback ispravka cookiesa

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5001;
-export const PUBLIC_BACKEND_IP = "/api";
+export const PUBLIC_BACKEND_IP = "https://dancearena-devtrak-backend.onrender.com";
 export const LOCAL_BACKEND_IP = `http://localhost:${PORT}`;
 export const BACKEND_IP = PUBLIC_BACKEND_IP;
