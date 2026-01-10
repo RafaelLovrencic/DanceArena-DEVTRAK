061fff11cc503da95587c87edcb737fba7c628e5
RafaelLovrencic <rafael.lovrencic@fer.unizg.hr>
2026-01-10 10:59:33 +0100

    Ispravak rute u config.js, novi vercel.json

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5001;
-export const PUBLIC_BACKEND_IP = "https://dance-arena-devtrak.vercel.app/api";
+export const PUBLIC_BACKEND_IP = "/api";
 export const LOCAL_BACKEND_IP = `http://localhost:${PORT}`;
 export const BACKEND_IP = PUBLIC_BACKEND_IP;
