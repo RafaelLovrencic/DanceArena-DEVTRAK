8c76806e4a261e004288dbace6c44eb897608951
EmanuelRod1 <emanuel.rod@fer.unizg.hr>
2026-01-11 17:30:11 +0100

    Promijenio da se frontend učiava s Rendera.

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 const FRONTEND_URL = process.env.NODE_ENV === "production"
-    ? "https://dance-arena-devtrak.vercel.app"
+    ? "https://dancearena-devtrak.onrender.com"
     : "http://localhost:5173";
 
 module.exports = { FRONTEND_URL };
