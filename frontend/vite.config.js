2fa170719616748222a73a81557373d31bfdaf52
alenjurina <alen.jurina@fer.hr>
2026-01-13 22:42:31 +0100

    Pokusaj popravka refresha

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // osigurava da build uzima root kao osnovu
 })
