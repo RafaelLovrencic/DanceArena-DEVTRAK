f43540154f7923b1853a5b20b77fe680d4fb5b42
alenjurina <alen.jurina@fer.hr>
2026-01-15 18:46:59 +0100

    Micanje Resenda

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -1,27 +1,47 @@
-const { sendEmail } = require("../../utils/mailer");
+const fetch = (...args) =>
+    import("node-fetch").then(({ default: fetch }) => fetch(...args));const { generirajPozivToken } = require("./token.service");
 const { generirajPozivToken } = require("./token.service");
-const { Resend } = require("resend");
-
-const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function posaljiPozivNaEmail(email, imeNatjecanja) {
-
-    const token = generirajPozivToken({
-        email: email,
-        imeNatjecanja: imeNatjecanja
-    });
-
+    const token = generirajPozivToken({ email, imeNatjecanja });
     const link = `https://dancearena-devtrak-backend.onrender.com/unospodataka/prijaviSuca?token=${token}`;
 
-    await resend.emails.send({
-        from: process.env.EMAIL_USER,
-        to: email,
+    const payload = {
+        sender: {
+            name: "DanceArena",
+            email: process.env.EMAIL_USER, 
+        },
+        to: [
+            {
+                email,
+            },
+        ],
         subject: "Dobrodošli u DanceArenu-DEVTRAK!",
-        html: 
-            `<h1>Hello ${email}</h1>
-            <p>Pozvani ste da sudjelete kao sudac u ocjenjivanju nastupa na natjecanju ${imeNatjecanja} 🎉</p>
-            <a href="${link}">Registriraj se!</a>`
+        htmlContent: `
+            <h1>Pozdrav ${email}</h1>
+            <p>Pozvani ste da sudjelujete kao sudac u ocjenjivanju nastupa na natjecanju <b>${imeNatjecanja}</b> 🎉</p>
+            <a href="${link}">Registriraj se!</a>
+        `,
+    };
+
+    const res = await fetch("https://api.brevo.com/v3/smtp/email", {
+        method: "POST",
+        headers: {
+            "accept": "application/json",
+            "content-type": "application/json",
+            "api-key": process.env.BREVO_API_KEY,
+        },
+        body: JSON.stringify(payload),
     });
+
+    const data = await res.json();
+
+    if (!res.ok) {
+        console.error("Brevo error:", data);
+        throw new Error("Greška pri slanju emaila");
+    }
+
+    console.log("Email poslan:", data);
 }
 
 module.exports = { posaljiPozivNaEmail };
