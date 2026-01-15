221e26dc556dfda405742e87f1c4900f48097131
alenjurina <alen.jurina@fer.hr>
2026-01-15 19:08:22 +0100

    Popravak redirecta s maila

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -25,6 +25,7 @@ router.get("/google/callback",
     try {
       if (!req.user) return res.redirect(FRONTEND_URL);
       const token = jwt.sign({ id: req.user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+      const state = req.query.state || "normal-login";
       res.redirect(`${FRONTEND_URL}/oauth-callback#token=${token}&state=${state}`);
     } catch (err) {
       console.error("Greška u callback-u:", err);
