66704fedd0b31385536c2933ae9ef0151929aac8
alenjurina <alen.jurina@fer.hr>
2026-01-19 20:55:07 +0100

    Popravak importa

--- frontend/src/stranice/OAuthCallback.jsx
+++ frontend/src/stranice/OAuthCallback.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { BACKEND_IP } from "../config";
 import { useAuth } from "../kontekst/AuthContext";
+import { generateFingerprint } from "../kontekst/fingerprint";
 
 
 export default function OAuthCallback() {
