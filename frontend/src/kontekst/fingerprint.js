21ae13779715f6333a76af522dba73f73263c639
alenjurina <alen.jurina@fer.hr>
2026-01-19 22:08:56 +0100

    Jos debug ispisa

--- frontend/src/kontekst/fingerprint.js
+++ frontend/src/kontekst/fingerprint.js
@@ -2,10 +2,9 @@ export function generateFingerprint() {
   const raw = [
     navigator.userAgent,                        // browser i OS
     navigator.language,                         // jezik
-    navigator.platform,                         // platforma (Win/Linux/Mac)
     navigator.hardwareConcurrency,              // broj CPU jezgri
     Intl.DateTimeFormat().resolvedOptions().timeZone // vremenska zona
   ].join("::");
-
+  console.log("Raw fingerprint string:", raw);
   return btoa(raw); 
 }
