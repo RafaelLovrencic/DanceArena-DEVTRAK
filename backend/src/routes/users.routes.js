0aca64cc850fdb85b8f1902726e7fcfc1eb20df9
EmanuelRod1 <emanuel.rod@fer.unizg.hr>
2026-01-21 14:31:00 +0100

    Dodao mogućnost brisanja i dohvaćanja korisnika.

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -14,4 +14,33 @@ router.get("/suci", async (req, res) => {
     }
 });
 
+
+router.get("/", async (req, res) => {
+    try {
+        const korisnicic = await User.find({ role: { $ne: "admin" } });
+        res.json(korisnicic);
+    }
+    catch (err) {
+        console.error("Greška pri dohvaćanju korisnika:", err);
+        res.status(500).json({ poruka: "Greška pri dohvaćanju korisnika" });
+    }
+});
+
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const deleted = await User.findByIdAndDelete(id);
+
+        if (!deleted) {
+            return res.status(404).json({ poruka: "Korisnik nije pronađen" });
+        }
+        res.json({ poruka: "Korisnik uspješno obrisano", natjecanje: deleted });
+
+    } catch (err) {
+        console.error("Greška pri brisanju korisnika:", err);
+        res.status(500).json({ poruka: "Greška pri brisanju korisnika" });
+    }
+}); 
+
 module.exports = router;
