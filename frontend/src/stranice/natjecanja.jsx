b3f8859a26c1bfc3f593f7e9cc79e185a5408a79
alenjurina <alen.jurina@fer.hr>
2026-01-21 11:37:54 +0100

    Micanje nepotrebne funkcije

--- frontend/src/stranice/natjecanja.jsx
+++ frontend/src/stranice/natjecanja.jsx
@@ -153,7 +153,6 @@ export default function Natjecanja() {
                 setVrijediDo(data.vrijediDo || null);
             } catch (err) {
                 console.error(err);
-                setGreska("Greška pri dohvaćanju statusa članarine");
             } finally {
                 setUcitavanje(false);
             }
